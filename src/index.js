4f68d767a8b3ce5ecef850229c0393a518dfaa99
AzatAza <ai.allagulov@yandex.ru>
2021-03-11 19:27:39 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0 ) n=-(n);
+    var b = 0;
+    var sum = 0;
+    while(n > 0) {
+      b = n % 10; 
+      sum = sum * 10 + b; 
+      n = parseInt(n / 10);}
+    return sum;
 }
